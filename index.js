510dd9f08ad8f320d30083e4c57b242c1a59da10
dekta <ektadhawal6306@gmail.com>
2022-11-10 22:40:43 +0530

    day-3

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import navbar from "./components/navbar.js";
 let navbar_div = document.getElementById("navbar");
 navbar_div.innerHTML = navbar();
+import Append from "./components/append.js"
 
+// import Append from "./components/Append.js"
+// let content_div = document.getElementById("content");
+// content_div.innerHTML = Append()
 // window.addEventListener('scroll',() => {
 
 //     if(window.scrollY >= 50){
@@ -12,12 +16,70 @@ navbar_div.innerHTML = navbar();
 //         navbar_div.classList.remove("active_navbar")
 //     }
 // })
-window.onscroll = function() {myFunction()};
-var sticky = navbar_div.offsetTop;
-function myFunction() {
-    if (window.pageYOffset >= sticky) {
-        navbar_div.classList.add("sticky")
-    } else {
-        navbar_div.classList.remove("sticky");
+// window.onscroll = function() {myFunction()};
+// var sticky = navbar_div.offsetTop;
+// function myFunction() {
+//     if (window.pageYOffset >= sticky) {
+//         navbar_div.classList.add("sticky")
+//     } else {
+//         navbar_div.classList.remove("sticky");
+//     }
+//   }
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 1.5,
+     spaceBetween: 10,
+    speed: 400,
+    centeredSlides: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+    autoplay: {
+        delay: 4000,
+      },
+    
+  });
+// swiper()
+// let line1_wrapper = document.querySelector(".swiper-wrapper")
+const RandomVideos = async () => {
+    try{
+        const API_KEY = 'AIzaSyAvXhy8R1ZBKbFsxM1eRSteQ-PcdlQ5NU0'
+        let res = await fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=12&key=${API_KEY}`)
+        let data = await res.json()
+        console.log(data.items)
+        let actual = data.items
+        Append(actual)
+        console.log(actual)
     }
-  }
+    catch(err){
+        console.log(err)
+    }
+}
+RandomVideos();
+
+// const Append = (data) =>{
+//     data.forEach((el)=>{
+//         let div = document.createElement("div")
+//         div.setAttribute("class","swiper-slide");
+//         let img = document.createElement('img');
+//         img.src = el.snippet.thumbnails
+//         div.append(div)
+//         line1_wrapper.append(div)
+//     })
+// }
---
+++ video_details.js
@@ -0,0 +1,24 @@
+import navbar from "./components/navbar.js";
+let navbar_div = document.getElementById("navbar");
+navbar_div.innerHTML = navbar();
+
+const show = ()=>{
+    let data = localStorage.getItem('c_video')
+    let {videoId,snippet}= JSON.parse(data)
+    //console.log(videoId)
+    //embedding a video usinfg iframe HTML element
+    let iframe = document.createElement('iframe')
+    iframe.src = `https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1`
+    iframe.width = '100%'
+    iframe.height = '100%'
+    iframe.setAttribute('allowfullscreen',true)
+   
+    let div = document.createElement('div')
+    div.setAttribute('class',"title")
+    div.innerText = snippet.title
+
+
+    let v_div = document.getElementById('video_details')
+    v_div.append(iframe,div)
+}
+show()
